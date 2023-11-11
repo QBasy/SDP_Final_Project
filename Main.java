9b85ea539f4ec1a6b63972b175cdae7d26635978
QoidynBasy <c0mplexie1@gmail.com>
2023-11-11 19:57:06 +0600

    sixth commit

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import Factory_CharecterCreator.*;
 import Observer.*;
 import Strategy_AttackType.Attack;
+import Strategy_AttackType.MageAttack;
 import Strategy_AttackType.PhysAttack;
 
 import java.util.ArrayList;
@@ -9,19 +10,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List<Human> allies = new ArrayList<>();
-        List<Human> enemy = new ArrayList<>();
-        enemy.add(new Wizard("enemy 1"));
-        enemy.add(new Warrior("enemy 2"));
         Scanner in = new Scanner(System.in);
 
+        List<Human> allies = new ArrayList<>();
+        List<Human> enemy = new ArrayList<>();
+
+        enemy.add(new Wizard("enemy 1"));
+        enemy.add(new Warrior("enemy 2"));
+
         System.out.println("How many Allies you want to create?");
-        int n = in.nextInt();
+        int n;
+        while (true) {
+            n = in.nextInt();
+            if (n > 3) {
+                System.out.println("Allies can't be more than 3");
+            } else {
+                break;
+            }
+        }
+
         System.out.println("Who you want to create?");
         for (int i = 0; i < n; i++) {
             System.out.println("1 - Warrior\n2 - Wizard\n3 - Priest");
             allies.add(createCharacter());
         }
+
         while (true) {
             if (allies.isEmpty()) {
                 System.out.println("You Lost!");
@@ -29,7 +42,7 @@ public class Main {
             }
             if (enemy.isEmpty()) {
                 System.out.println("You Won!");
-                System.exit(0);
+                break;
             }
             System.out.println("Make your move");
             move(allies, enemy);
@@ -68,7 +81,6 @@ public class Main {
             int n = in.nextInt();
             boolean loop = true;
             while (loop) {
-                int num;
                 switch (n) {
                     case 1:
                         Attack attack = new Attack();
@@ -81,6 +93,16 @@ public class Main {
                                     System.out.println((j+1) + " - " + enemies.get(j).getName());
                                 }
                                 attack.attack(allies.get(i), enemies.get(in.nextInt() - 1));
+                                System.out.println();
+                                break;
+                            case 2:
+                                attack.setAttackType(new MageAttack());
+                                System.out.println("Which person?");
+                                for (int j = 0; j < enemies.size(); j++) {
+                                    System.out.println((j+1) + " - " + enemies.get(j).getName());
+                                }
+                                attack.attack(allies.get(i), enemies.get(in.nextInt() - 1));
+                                System.out.println();
                                 break;
                         }
                         loop = false;
@@ -91,6 +113,7 @@ public class Main {
                             System.out.println((j+1) + " - " + allies.get(j).getName());
                         }
                         allies.get(i).giveHeal(allies.get(in.nextInt() - 1));
+                        System.out.println();
                         loop = false;
                         break;
                     case 3:
@@ -99,12 +122,14 @@ public class Main {
                             System.out.println((j+1) + " - " + allies.get(j).getName());
                         }
                         allies.get(i).giveMana(allies.get(in.nextInt() - 1));
+                        System.out.println();
                         loop = false;
                         break;
                     case 4:
                         loop = false;
                     default:
                         System.out.println("FATAL ERROR!!! TRY AGAIN!!!");
+                        System.out.println();
                 }
             }
         }
@@ -112,8 +137,11 @@ public class Main {
 
     public static void update(List<Human> allies, List<Human> enemy) {
         Observer observer = new ObserverClass();
-        for (int i = 0; i < allies.size(); i++) {
-            observer.update(allies.get(i));
+        for (Human ally : allies) {
+            observer.update(ally);
+        }
+        for (Human human : enemy) {
+            observer.update(human);
         }
     }
 }
